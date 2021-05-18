54d53bbb43baf9ef45103a9431922ba001ef231e
RubenV-Dev <r.will16@hotmail.com>
2021-05-18 15:43:55 -0400

    linkedList and tests all passing moved over from a different local directory

---
+++ app/src/test/java/data/structures/and/algos/NodeTest.java
@@ -0,0 +1,27 @@
+package data.structures.and.algos;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class NodeTest {
+    @Test
+    @DisplayName("Can create a node with integer")
+    void nodeShouldCreatedWithInteger() {
+        Node node = new Node(10);
+        int expected = 10;
+        int actual = node.getData();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("New node's next node is null")
+    void nodeShouldCreatedWithNextNullPointer() {
+        Node node = new Node(10);
+        Node actual = node.getNext();
+        assertNull(actual);
+    }
+
+}
